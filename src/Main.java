f26808ab7ff6ffc23d3ace884d6ae88f04b91dfe
Eliakim Paulino França <eliakim.paulino@gmail.com>
2026-05-29 14:58:59 -0300

    refactor Main.java to change input prompt and sum integers instead of strings

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Digite uma palavra:");
+        System.out.println("Digite um número:");
         Scanner sc = new Scanner(System.in);
-        String x;
-        x = sc.next();
+        int x = sc.nextInt();
+        int sum = 0;
 
-        System.out.println("Você digitou: " + x);
- 
+        while (x != 0) {
+            sum += x;
+            System.out.println("Digite outro número:");
+            x = sc.nextInt();
+        }
+
+        System.out.println("A soma dos números digitados é: " + sum);
         sc.close();
 
     }
