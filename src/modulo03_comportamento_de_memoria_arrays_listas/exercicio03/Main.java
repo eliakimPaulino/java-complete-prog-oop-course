550e5b5cd8a93bdaeacdf4d353754ac943b5cdf4
Eliakim Paulino França <eliakim.paulino@gmail.com>
2026-06-02 19:56:34 -0300

    add: implement Main class for inputting numbers and displaying negative values

---
+++ src/modulo03_comportamento_de_memoria_arrays_listas/exercicio03/Main.java
@@ -0,0 +1,34 @@
+package modulo03_comportamento_de_memoria_arrays_listas.exercicio03;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos números você deseja inserir? ");
+        int numero = sc.nextInt();
+
+        int[] array = new int[numero];
+        int[] negativeNumber = new int[numero];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Digite o " + (i + 1) + "° número: ");
+            array[i] = sc.nextInt();
+            if (array[i] < 0) {
+                negativeNumber[i] = array[i];
+            }
+        }
+
+        System.out.println("Números negativos: ");
+
+        for (int i = 0; i < negativeNumber.length; i++) {
+            if (negativeNumber[i] != 0) {
+                System.out.println(negativeNumber[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
